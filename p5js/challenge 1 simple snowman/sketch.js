cb7db9f720ce4f37e4ccc438b26298e5d76c1a4f
Luc de wit <luc.dewit@hotmail.nl>
2019-09-13 11:50:02 +0200

    added p5js challenge 1 and 2

---
+++ p5js/challenge 1 simple snowman/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+	createCanvas(400, 400);
+	ellipse(200, 300, 150, 150);
+	ellipse(200, 210, 100, 100);
+	ellipse(200, 150, 70, 70);
+}
+
+function draw(){
+
+}
---
+++ p5js/challenge 2 waving snowman/sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+	createCanvas(400, 400);
+
+	rect(0, 300, 400, 100);
+	ellipse(200, 300, 150, 150);
+	ellipse(200, 210, 100, 100);
+	ellipse(200, 150, 70, 70);
+	line(160, 200, 88, 91);
+	line(240, 200, 293, 91);
+}
+
+function draw(){
+
+}
