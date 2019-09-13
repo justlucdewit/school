7bf5bb58f74e520d87eef19bec288e12db515205
Luc de wit <luc.dewit@hotmail.nl>
2019-09-13 19:27:29 +0200

    fixed map placement error

---
+++ periode 1/p5js/challenge 3 sunny snow day/sketch.js
@@ -0,0 +1,24 @@
+function setup(){
+	createCanvas(400, 400);
+
+	background(0, 150, 150);
+
+
+	//the ground
+	fill(0, 255, 0);
+	rect(0, 300, 400, 100);
+
+	//the sun
+	fill(255, 255, 0)
+	ellipse(80, 64, 100, 100);
+
+	//the snow man
+	fill(255)
+	ellipse(200, 300, 150, 150);
+	ellipse(200, 200, 100, 100);
+	ellipse(200, 120, 75, 75);
+}
+
+function draw(){
+
+}
---
+++ periode 1/p5js/challenge 5 funky frog/sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+	createCanvas(400, 400);
+	ellipse(150, 70, 60, 120);//left ear
+	ellipse(240, 70, 60, 120);//right ear
+
+	ellipse(200, 170, 150, 150); //face
+	
+	fill(0, 0, 0);
+
+	let eyeSize = 40;
+	ellipse(170, 150, eyeSize, eyeSize);//left eye
+	ellipse(230, 150, eyeSize, eyeSize);//right eye
+
+	line(150, 200, 250, 200);//mouth
+
+	noFill();
+
+	let teethHeight = 100;
+	rect(185, 200, 15, teethHeight);//left tooth
+	rect(200, 200, 15, teethHeight);//right tooth
+}
+
+function draw(){
+
+}
