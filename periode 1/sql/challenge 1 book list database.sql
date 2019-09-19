87bd876f531685894a47bed50a3761668f025826
Luc de wit <luc.dewit@hotmail.nl>
2019-09-19 11:24:06 +0200

    added sql 1&2

---
+++ periode 1/sql/challenge 1 book list database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books(id integer primary key, name TEXT, rating INTEGER);
+
+INSERT into books values(1, "Ready player one", 9);
+INSERT into books values(2, "harry potter", 7);
+INSERT into books values(3, "harry snotter", 6);
+SELECT * FROM books;
