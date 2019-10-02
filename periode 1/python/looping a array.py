0b5d53b269e0e7f386055243bada7fa9c074b376
Luc de wit <luc.dewit@hotmail.nl>
2019-10-02 10:19:23 +0200

    added htmljs folder

---
+++ periode 1/python/looping a array.py
@@ -0,0 +1,9 @@
+array = ["one", "two", "tree", "four", "five", "six"]
+
+for element in array:
+	print(element)
+
+input()
+
+#let array = ["one", "two", "tree", "four", "five", "six"];
+#for (i in array){console.log(array[i]);}
