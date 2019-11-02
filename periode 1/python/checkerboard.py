b93fce2d1a67884d8db16969f25db71a2f852c83
Luc de wit <luc.dewit@hotmail.nl>
2019-11-02 16:49:30 +0100

    deleted py and english

--- periode 1/python/checkerboard.py
+++
@@ -1,5 +0,0 @@
-#epic multiplication table 3000
-
-n=7
-for i in range(100):
-	print(f"{i}x{n}={i*n}")
--- periode 1/python/looping a array.py
+++
@@ -1,9 +0,0 @@
-array = ["one", "two", "tree", "four", "five", "six"]
-
-for element in array:
-	print(element)
-
-input()
-
-#let array = ["one", "two", "tree", "four", "five", "six"];
-#for (i in array){console.log(array[i]);}
