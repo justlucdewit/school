69bbb4d3a37f0136063647d52bfa652ef9020d32
Luc de wit <luc.dewit@hotmail.nl>
2019-11-11 13:56:10 +0100

    added piere opdracht 2

---
+++ periode 2/PHP/opdrachten van piere/tafel van 9/tafelvan9.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>hello world</title>
+    </head>
+
+    <body>
+		<?php
+			$tafelvan = 9;
+			$tot = 100;
+
+			for ($i = 0; $i<=$tot; $i++){
+				echo "$tafelvan x $i = ".$tafelvan*$i."<br>";
+			}
+        ?>
+    </body>
+</html>
