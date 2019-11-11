12a5fe16edda81fea202f328c08b10c29c11d406
Luc de wit <luc.dewit@hotmail.nl>
2019-11-11 15:05:37 +0100

    added php opdr 4.4.2

---
+++ periode 2/PHP/opdrachten uit het boek/php4.4.2/index.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        <title>hello world</title>
+    </head>
+
+    <body>
+		<?php
+			switch (date("H")){
+                case 0:
+                    $dagdeel = "nacht";
+                    break;
+                case 1:
+                    $dagdeel = "nacht";
+                    break;
+                case 2:
+                    $dagdeel = "nacht";
+                    break;
+                case 3:
+                    $dagdeel = "nacht";
+                    break;
+                case 4:
+                    $dagdeel = "nacht";
+                    break;
+                case 5:
+                    $dagdeel = "nacht";
+                    break;
+                case 6:
+                    $dagdeel = "ochtend";
+                    break;
+                case 7:
+                    $dagdeel = "ochtend";
+                    break;
+                case 8:
+                    $dagdeel = "ochtend";
+                    break;
+                case 9:
+                    $dagdeel = "ochtend";
+                    break;
+                case 10:
+                    $dagdeel = "ochtend";
+                    break;
+                case 11:
+                    $dagdeel = "ochtend";
+                    break;
+                case 12:
+                    $dagdeel = "middag";
+                    break;
+                case 13:
+                    $dagdeel = "middag";
+                    break;
+                case 14:
+                    $dagdeel = "middag";
+                    break;
+                case 15:
+                    $dagdeel = "middag";
+                    break;
+                case 16:
+                    $dagdeel = "middag";
+                    break;
+                case 17:
+                    $dagdeel = "middag";
+                    break;
+                case 18:
+                    $dagdeel = "avond";
+                    break;
+                case 19:
+                    $dagdeel = "avond";
+                    break;
+                case 20:
+                    $dagdeel = "avond";
+                    break;
+                case 21:
+                    $dagdeel = "avond";
+                    break;
+                case 23:
+                    $dagdeel = "avond";
+                    break;
+                case 24:
+                    $dagdeel = "avond";
+                    break;
+            }
+			echo "het is nu $dagdeel"
+        ?>
+    </body>
+</html>
