84506065f14a9383c411837bcb9ee1cd9ba0b52a
Luc de wit <luc.dewit@hotmail.nl>
2019-11-12 09:44:37 +0100

    php 4.4.6

---
+++ periode 2/PHP/opdrachten uit het boek/php4.4.7/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>hello world</title>
+    </head>
+
+    <body>
+		<?php
+            $spaargeld = 69420;
+            if ($spaargeld < 100000){
+                $verschil = 1000-$spaargeld;
+                echo "Helaas, je hebt $verschil euro tekort om een iphone te kopen";
+            }else{
+                $over = $spaargeld - 1000;
+                echo "je kan de iphone kopen, je hebt dan $over euro over";
+            }
+        ?>
+    </body>
+</html>
