72e1c0342e163dd3d68edc5ff6fc97ae1c30fee1
Luc de wit <luc.dewit@hotmail.nl>
2019-11-12 14:08:57 +0100

    php 5.3.2

---
+++ periode 2/PHP/opdrachten uit het boek/hoofdstuk 5/php5.3/php5.3.2.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>e benaderen</title>
+    </head>
+    <body>
+        <?php
+            $limiter = 100000;
+            $iteration = 0;
+            $pi = 1;//3.14159265358979323846264
+            $opp = true;
+            for ($n = 3; $n<$limiter; $n+=2){
+                if ($opp){
+                    $pi -= 1/$n;
+                    $opp = false;
+                }else{
+                    $pi += 1/$n;
+                    $opp = true;
+                }
+                $iteration++;
+            }
+            echo $pi*4;
+        ?>
+    </body>
+</html>
