017238078bbc6c95210f16741faa2adc185e8982
Luc de wit <luc.dewit@hotmail.nl>
2019-11-14 10:21:08 +0100

    7.6.4

---
+++ periode 2/PHP/opdrachten uit het boek/hoofdstuk 7/php7.6.4.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title></title>
+        <style>
+            div{
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post" action="">
+            <label>Prijs </label><input type="text" name="prijs"><br>
+            <label>Korting(%) </label><input type="text" name="korting"><br>
+            <input type="submit" value="verander achtergrond" name="submit">
+        </form>
+        <?php
+            if(isset($_POST["submit"])){
+                $prijs = $_POST["prijs"];
+                $korting = $_POST["korting"];
+                $korting = (intval($korting)/100);
+                $prijs = intval($prijs)-(intval($prijs)*$korting);
+                $korting = ($korting)*100;
+                echo "bedrag inclusied ".$korting."% korting: €".number_format($prijs, 2, ",", ".");
+            }
+        ?>
+    </body>
+</html>
